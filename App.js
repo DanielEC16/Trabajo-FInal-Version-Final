86ed46390b53ba5e8833617661f72df3eae90eb6
Daniel Cairo <danielcairo695@gmail.com>
2023-02-15 22:31:37 -0500

    Modificaciones en algunos archivos

---
+++ App.js
@@ -0,0 +1,12 @@
+var modal = document.getElementById("mi-modal");
+var closeBtn = modal.querySelector(".close");
+
+closeBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
